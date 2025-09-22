6331e7fd096f631466b0fbf52d99801df5551afb
alex.silvak23 <alex.silvak23@gmail.com>
2025-09-22 00:28:14 -0300

    feat(api): criar rotas iniciais para módulos principais

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BanksModule } from './banks/banks.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { MovementsModule } from './movements/movements.module';
+import { CategoriesModule } from './categories/categories.module';
+import { GoalsModule } from './goals/goals.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/finbank'), // seu banco
-    UsersModule,
+    UsersModule, BanksModule, TransactionsModule, AccountsModule, MovementsModule, CategoriesModule, GoalsModule, ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
